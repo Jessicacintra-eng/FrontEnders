554a9d0248820435abed2b7a0b8d9e8be8c98d78
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-04-24 23:44:47 -0300

    fix bug header

--- cabecalho_e_rodape/cabecalhoErodape.js
+++ cabecalho_e_rodape/cabecalhoErodape.js
@@ -1,5 +1,12 @@
-$("#btn-login").click((e)=>{
+$("#btn-login").click(function (e){
     e.preventDefault();
-    window.location.assign("./pagina_de_login.html"); 
+    console.log("oi")
+    window.location.assign("../pagina_de_login/pagina_de_login.html");
+     
 }
 )
+$("#link-home").click((e) => {
+    e.preventDefault();
+    console.log("oi")
+    window.location.assign("../index.htmll"); 
+})

9f190b917b283c011eb4c4b25ddf6ab8a7d58854
Jessica Cintra <jessica.cintra0712@gmail.com>
2022-04-29 11:26:42 -0300

    Last commit: rodapé e botoes

--- cabecalho_e_rodape/cabecalhoErodape.js
+++ cabecalho_e_rodape/cabecalhoErodape.js
@@ -18,3 +18,37 @@ $(document).ready(function() {
 
 
 });
+$("#btn-login-index").click((e)=>{
+    e.preventDefault()
+    window.location.assign("./pagina_de_login/pagina_de_login.html");
+})
+$("#btn-login").click((e)=>{
+    e.preventDefault()
+    window.location.assign("../pagina_de_login/pagina_de_login.html");
+})
+$("#btn-cad-index").click((e)=>{
+    e.preventDefault()
+    window.location.assign("./pag_cadastro/pag_cadastro.html");
+})
+$("#btn-cad").click((e)=>{
+    e.preventDefault()
+    window.location.assign("../pag_cadastro/pag_cadastro.html");
+})
+$("#btn-form").click((e)=>{
+    e.preventDefault()
+    window.location.assign("../pag_assinaturas/assinaturas.html");
+})
+$("#btn-assinatura").click((e)=>{
+    e.preventDefault()
+    window.location.assign("../pag_assinaturas/assinaturas.html");
+})
+const botoesAssinatura=document.querySelectorAll(".btn-assinatura")
+botoesAssinatura.forEach((botaoAssinatura) => {
+    botaoAssinatura.addEventListener("click", (e) => {
+        e.preventDefault();
+        swal("Entendido!", "Aguarde mais informações no seu e-mail. Obrigada por escolher a FrontEnders.");
+      });
+    
+});
+
+
